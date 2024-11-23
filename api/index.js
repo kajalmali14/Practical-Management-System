b184ddcdf104e31ec7ae90d5014528fd6daee0f4
kajalmali14 <kajalkmali111@gmail.com>
2024-11-23 16:58:50 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import router from "./routers/feedbackRouter.js";
-import dbConnect from "./config/database.js";
+import router from "../routers/feedbackRouter.js";
+import dbConnect from "../config/database.js";
 
 
 dotenv.config();
