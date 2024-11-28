8695799f8b1d17b3625c13dccb78aede46417d6c
kajalmali14 <kajalkmali111@gmail.com>
2024-11-28 11:55:50 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -17,9 +17,16 @@ app.use(express.json());
 app.use("/api/v1", router);
 
 dbConnect();
-app.get("/",(req,res)=>{
-  res.send("Server Running Successfully");
-});
+
+app.get("/", (req, res)=>{
+  res.json({
+    success: true,
+    message: "Server Running Successfully"
+  })
+})
+// app.get("/",(req,res)=>{
+//   res.send("Server Running Successfully");
+// });
 app.listen(PORT, () => {
   console.log(`Server is running at port: ${PORT}`);
 });
